9906a6a0f3bdeb06e805c8ecb7424e45cc695a2c
Alexander Rybakov <rybakov@interaktiv.de>
2026-02-03 14:20:11 +0100

    Update namespaces, replace deprecated pkg_resources

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 import sys
 from os import path
 
-from pkg_resources import Requirement, parse_version
+from packaging.specifiers import SpecifierSet
+from packaging.version import Version
 from setuptools import find_packages, setup
 
 # Package metadata
@@ -38,8 +39,8 @@ except FileNotFoundError:
 
 # Check required python version
 def check_python_version():
-    required_python = Requirement.parse("python" + REQUIRES_PYTHON)
-    current_version = parse_version(".".join(map(str, sys.version_info[:3])))
+    required_python = SpecifierSet(REQUIRES_PYTHON)
+    current_version = Version(".".join(map(str, sys.version_info[:3])))
     if current_version not in required_python:
         sys.exit(
             f"'{NAME}' requires Python {REQUIRES_PYTHON} but the current Python is {current_version}"
@@ -67,9 +68,6 @@ setup(
     license="proprietary",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    namespace_packages=[
-        "interaktiv",
-    ],
     include_package_data=True,
     zip_safe=False,
     python_requires=check_python_version(),
